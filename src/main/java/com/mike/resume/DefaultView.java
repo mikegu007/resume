9c4804618614de8cafbcb5c6b976bcd5e084ba89
mikegu007 <273210563@qq.com>
2018-03-23 19:05:57 +0800

    默认页

--- src/main/java/com/mike/resume/DefaultView.java
+++ src/main/java/com/mike/resume/DefaultView.java
@@ -1,17 +1,17 @@
-//package com.mike.resume;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-///**
-// * Created by dell on 2018/3/21.
-// */
-//@Configuration
-//public class DefaultView extends WebMvcConfigurerAdapter {
-//    @Override
-//    public void addViewControllers( ViewControllerRegistry registry ) {
-//        registry.addViewController( "/" ).setViewName( "forward:/hi" );
-//        super.addViewControllers( registry );
-//    }
-//}
+package com.mike.resume;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by dell on 2018/3/21.
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers( ViewControllerRegistry registry ) {
+        registry.addViewController( "/" ).setViewName( "forward:/hi" );
+        super.addViewControllers( registry );
+    }
+}
